d6ad24a332c1723984dab17f64f4f8c43891da3c
Takahiro Ishida <takahiro.ishida@heathrow.co.jp>
2015-02-27 08:06:40 +0000

    ファッション辞典のdescriptionも取ってこれる様に変更

--- sqrape_iqon_dic.rb
+++ sqrape_iqon_dic.rb
@@ -7,11 +7,17 @@ require 'pp'
 class SQ_IQON_DIC
   def get_dic_page
     array = []
+    temp = []
+    description_arr = []
     gen_attack_url.each do |target|
       begin
         agent = Mechanize.new
         source = agent.get(target)
-        array << source.title
+        description = source.search('div.word_definition').search('p').text
+        temp << source.title.gsub(/\//, "")
+        temp << description
+        description_arr << temp.pop(2)
+        array << source.title.gsub(/\//, "")
       rescue Mechanize::ResponseCodeError => e
         case e
         when "404"
@@ -20,6 +26,7 @@ class SQ_IQON_DIC
       end
     end
     gen_csv('./temp/temp_iqon.csv', array)
+    gen_csv('./temp/temp_iqon_des.csv', description_arr)
   end
 
   def gen_attack_url
@@ -31,17 +38,23 @@ class SQ_IQON_DIC
     return array
   end
 
+  def description_title_modification
+    load_temp_csv('./temp/temp_iqon_des.csv').each do |element|
+      pp element.gsub(/"/, "")
+    end
+  end
+
   def name_picker
     array = []
-    load_temp_csv.each do |n|
+    load_temp_csv('./temp/temp_iqon.csv').each do |n|
       n.slice!("とは - ファッション辞書 | iQON-レディースファッションアプリ")
       array << n
     end
     gen_csv('./csv/keyword_iqon_dic.csv', array)
   end
 
-  def load_temp_csv
-    return CSV.read('./temp/temp_iqon.csv').flatten
+  def load_temp_csv(target_csv)
+    return CSV.read(target_csv).flatten
   end
 
   def gen_csv(save_dir_name, content)
@@ -52,5 +65,6 @@ class SQ_IQON_DIC
 end
 
 sq_iqon_dic = SQ_IQON_DIC.new
-sq_iqon_dic.get_dic_page
-sq_iqon_dic.name_picker
+#sq_iqon_dic.get_dic_page
+#sq_iqon_dic.name_picker
+sq_iqon_dic.description_title_modification
